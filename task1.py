107be507651e9568e6f12e525a9b83a70a5e0a0c
j.pikin <pikin@bk.ru>
2023-06-27 11:24:31 +0500

    task1 added

---
+++ task1.py
@@ -0,0 +1,12 @@
+
+total = []
+lst = ['222anton456', 'a1n1t1o1n1', '0000a0000n00t00000o000000n', 'gylfole', 'richard', 'ant0n']
+for j in range(len(lst)):
+    for i in 'anton':
+        if i not in lst[j]: break
+        else:
+            lst[j] = lst[j][lst[j].index(i):]
+            total.append(lst[j][0])
+    if 'anton' in ''.join(total):
+        print(j + 1, end = ' ')
+        total.clear()
