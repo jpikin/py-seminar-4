cc472acddf5a2b28446bf3ae70053336f314352d
j.pikin <pikin@bk.ru>
2023-06-29 15:33:28 +0500

    task1 fixed

--- task1.py
+++ task1.py
@@ -1,7 +1,7 @@
 
-total = []
 lst = ['222anton456', 'a1n1t1o1n1', '0000a0000n00t00000o000000n', 'gylfole', 'richard', 'atnon']
 for j in range(len(lst)):
+    total = []
     for i in 'anton':
         if i not in lst[j]: break
         else:
@@ -9,4 +9,3 @@ for j in range(len(lst)):
             total.append(lst[j][0])
     if 'anton' in ''.join(total):
         print(j + 1, end = ' ')
-    total.clear()
