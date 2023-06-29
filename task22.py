d9db19a8d7ea54e6981646c4d00704645356ff9b
j.pikin <pikin@bk.ru>
2023-06-29 12:42:27 +0500

    task22 added 2 var

--- task22.py
+++ task22.py
@@ -4,9 +4,32 @@
 # Пользователь вводит 2 числа. n — кол-во элементов первого множества. m — кол-во элементов второго множества. 
 # Затем пользователь вводит сами элементы множеств.
 
+from random import randint
 
 def set_create(x):
-    return {int(input()) for _ in range(x)}
+    new_set = {randint(1,10) for _ in range(x)}
+    print(f'Множество {new_set}')
+    return new_set
 
-n, m = int(input()), int(input())
-print(*sorted(set_create(n) & set_create(m)))
+n, m = int(input("Введите размер первого множества ")), int(input("Введите размер второго множества "))
+
+sorted_lst = sorted(set_create(n) & set_create(m))
+if len(sorted_lst) > 0:
+    print("Общие элементы множеств:", end=' ')
+    print(*sorted_lst)
+else: print("Общих элементов не найдено")
+
+
+
+
+
+
+
+# Вариант 2:
+
+# def set_create(x):
+#     return {int(input()) for _ in range(x)}
+
+# n, m = int(input()), int(input())
+# print(*sorted(set_create(n) & set_create(m)))
+
